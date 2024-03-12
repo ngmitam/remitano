79b94acf7dfce4da3410fc0a639597fee984ecbd
Nguyễn Minh Tâm <ngmitamit@gmail.com>
2024-03-12 19:38:37 +0700

    initialize

--- programs/remitano/src/lib.rs
+++ programs/remitano/src/lib.rs
@@ -1,15 +1,100 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
+
+
 
 declare_id!("E1CRjpkK9JyHhNvSFeVy1BgQSJx1CPZQuGfnrXR8Sbs2");
+
+pub const RATE : u64 = 10;
 
 #[program]
 pub mod remitano {
     use super::*;
 
+    #[error_code]
+    pub enum ErrorCode {
+        #[msg("Pool is already initialized")]
+        PoolAlreadyInitialized,
+    }
+
+    // set up the pool SOL <-> MOVE
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // check if the pool is already initialized
+        let pool_state=  &mut  ctx.accounts.pool_state;
+        if pool_state.is_initialized {
+            return Err(ErrorCode::PoolAlreadyInitialized.into());
+        }
+
+        // initialize the pool
+        pool_state.is_initialized = true;
         Ok(())
     }
 }
 
+#[account]
+#[derive()] 
+pub struct PoolState {
+    pub is_initialized: bool,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    pub move_token: Account<'info, Mint>,
+    #[account(
+        init,
+        space = 8 + 1,
+        payer=payer, 
+        seeds=[b"pool_state", move_token.key().as_ref()], 
+        bump,
+    )]
+    pub pool_state: Box<Account<'info, PoolState>>,
+
+    #[account(seeds=[b"authority", pool_state.key().as_ref()], bump)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub pool_authority: AccountInfo<'info>,
+
+    // account to hold MOVE tokens
+    #[account(
+        init, 
+        payer=payer, 
+        seeds=[b"vault0", pool_state.key().as_ref()], 
+        bump,
+        token::mint = move_token,
+        token::authority = pool_authority
+    )]
+    pub vault0: Box<Account<'info, TokenAccount>>, 
+
+    // account to hold SOL tokens
+    #[account(
+        init,
+        payer=payer,
+        seeds=[b"vault1", pool_state.key().as_ref()],
+        bump,
+        token::mint = move_token,
+        token::authority = pool_authority
+    )]
+    pub vault1: Box<Account<'info, TokenAccount>>,
+
+    // account to hold the pool mint
+    #[account(
+        init, 
+        payer=payer,
+        seeds=[b"pool_mint", pool_state.key().as_ref()], 
+        bump, 
+        mint::decimals = 9,
+        mint::authority = pool_authority
+    )] 
+    pub pool_mint: Box<Account<'info, Mint>>, 
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    // accounts required to init a new mint
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
--- tests/remitano.ts
+++ tests/remitano.ts
@@ -1,16 +1,92 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Remitano } from "../target/types/remitano";
+import * as token from "@solana/spl-token";
 
 describe("remitano", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	// Configure the client to use the local cluster.
+	let provider = anchor.AnchorProvider.env();
+	let connection = provider.connection;
+	anchor.setProvider(provider);
 
-  const program = anchor.workspace.Remitano as Program<Remitano>;
+	const program = anchor.workspace.Remitano as Program<Remitano>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	let pool;
+	let n_decimals = 9;
+
+	it("Is initialized!", async () => {
+		let auth = anchor.web3.Keypair.generate();
+		let sig = await connection.requestAirdrop(
+			auth.publicKey,
+			100 * anchor.web3.LAMPORTS_PER_SOL
+		);
+		await connection.confirmTransaction(sig);
+
+		let moveToken = await token.createMint(
+			connection,
+			auth,
+			auth.publicKey,
+			auth.publicKey,
+			n_decimals
+		);
+
+		let [poolState, poolState_b] =
+			anchor.web3.PublicKey.findProgramAddressSync(
+				[Buffer.from("pool_state"), moveToken.toBuffer()],
+				program.programId
+			);
+
+		let [poolAuthority, poolAuthority_b] =
+			anchor.web3.PublicKey.findProgramAddressSync(
+				[Buffer.from("authority"), poolState.toBuffer()],
+				program.programId
+			);
+
+		let [vault0, vault0_b] = anchor.web3.PublicKey.findProgramAddressSync(
+			[Buffer.from("vault0"), poolState.toBuffer()],
+			program.programId
+		);
+
+		let [vault1, vault1_b] = anchor.web3.PublicKey.findProgramAddressSync(
+			[Buffer.from("vault1"), poolState.toBuffer()],
+			program.programId
+		);
+
+		let [poolMint, poolMint_b] =
+			anchor.web3.PublicKey.findProgramAddressSync(
+				[Buffer.from("pool_mint"), poolState.toBuffer()],
+				program.programId
+			);
+
+		await program.rpc
+			.initialize({
+				accounts: {
+					moveToken,
+					poolState,
+					poolAuthority,
+					vault0,
+					vault1,
+					poolMint,
+					payer: auth.publicKey,
+					systemProgram: anchor.web3.SystemProgram.programId,
+					tokenProgram: token.TOKEN_PROGRAM_ID,
+					associatedTokenProgram: token.ASSOCIATED_TOKEN_PROGRAM_ID,
+					rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+				},
+				signers: [auth],
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+
+		pool = {
+			moveToken,
+			poolState,
+			poolAuthority,
+			vault0,
+			vault1,
+			poolMint,
+			auth,
+		};
+	});
 });
